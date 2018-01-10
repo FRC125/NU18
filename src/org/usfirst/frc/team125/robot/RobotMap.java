eb1524e0393871b1b8ec2aea6bf8abe430db695d
Josh Young <joshayoung125@gmail.com>
2018-01-10 18:33:25 -0500

    commenting...

--- src/org/usfirst/frc/team125/robot/RobotMap.java
+++ src/org/usfirst/frc/team125/robot/RobotMap.java
@@ -8,7 +8,7 @@
 package org.usfirst.frc.team125.robot;
 
 public class RobotMap {
-    //CAN Devices
+    //CAN Devices TODO: Make sure these are correspondant to the actual drivetrain motors on webdash
     public static final int LEFT_DRIVE_A = 0;
     public static final int LEFT_DRIVE_B = 1;
     public static final int RIGHT_DRIVE_A = 2;
