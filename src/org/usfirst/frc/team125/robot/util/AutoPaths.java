19769335539fbd5e1836a714a52f829e53f17798
Josh Young <JoshAYoung125@gmail.com>
2018-01-27 16:32:23 -0800

    tuned' '

--- src/org/usfirst/frc/team125/robot/util/AutoPaths.java
+++ src/org/usfirst/frc/team125/robot/util/AutoPaths.java
@@ -18,7 +18,7 @@ public class AutoPaths {
 
     public static Waypoint[] wallToSwitch = new Waypoint[] {
             new Waypoint(0.0, 0.0, Pathfinder.d2r(0.0)),
-            new Waypoint(5.125, 0.0, Pathfinder.d2r(0.0)), // 1:3.53568 1/2:1.77 1/4:2.63 !:!:! 5.125
+            new Waypoint(5.125, 0.0, Pathfinder.d2r(0.0)), // 1:3.53568 1/2:1.77 1/4:2.63 !:!:! 5.125!
 };
 
 }
